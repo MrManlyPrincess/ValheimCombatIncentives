7c68affeb121d5ad27bd6f5a94bdda8c275537e5
Gavin <Neyen13@gmail.com>
2022-06-04 14:55:42 -0500

    Initial commit

---
+++ ValheimWards/Extensions/Vector3Extensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace ValheimWards.Extensions
+{
+    public static class Vector3Extensions
+    {
+        public static bool WithinDistance(this Vector3 origin, Vector3 pointToTest, double distance) => Vector3.Distance(origin, pointToTest) <= distance;
+    }
+}
